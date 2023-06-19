82824129a7ae70e3e1f4fc16d5c867901e4367e7
Yusuf Büyükdağ <123562650+ybuyukdag@users.noreply.github.com>
2023-06-19 11:58:55 +0300

    Update openai_sample.py

--- openai_sample.py
+++ openai_sample.py
@@ -18,3 +18,7 @@ while True:
     reply = chat.choices[0]["message"]["content"]
     print("ChatGPT: "+reply)
     messages.append({"role":"assistant","content": reply})
+
+
+
+####Chat-GPT-3.5 has no access to internet anymore.ITs' last access date is 2021. So, searching about 2022 and next will be unsuccessful. 
